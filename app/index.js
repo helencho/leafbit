626e33d1ad7a205b1b2b10b906d7d5568d388e28
helencho <helencho@ac.c4q.nyc>
2018-05-19 14:30:28 -0400

    update tree based on progress

--- app/index.js
+++ app/index.js
@@ -1,16 +1,16 @@
 import clock from "clock";
+import { goals, today } from "user-activity";
+import { display } from "display";
 import document from "document";
 import { preferences } from "user-settings";
 import * as util from "../common/utils";
 
+// DISPLAY CLOCK
 // Update the clock every minute
 clock.granularity = "minutes";
 
 // Get a handle on the <text> element
 const myLabel = document.getElementById("time");
-
-let tree = document.getElementById("tree");
-tree.href= "../resources/assets/healthy_growing.png"
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
@@ -23,8 +23,16 @@ clock.ontick = (evt) => {
     // 24h format
     hours = util.zeroPad(hours);
   }
-  
-  
   let mins = util.zeroPad(today.getMinutes());
   myLabel.text = `${hours}:${mins}`;
 }
+
+
+// DISPLAY TREE 
+// Define goal. This would come from the seed user chose 
+const goalSteps = goals.steps || 0;
+const localSteps = today.local.steps || 0;
+const progress = (localSteps / goalSteps) * 100;
+
+// Update step progress 
+util.updateSteps(progress);
--- common/utils.js
+++ common/utils.js
@@ -1,7 +1,34 @@
+import { display } from "display";
+
 // Add zero in front of numbers < 10
-export function zeroPad(i) {
+export const zeroPad = (i) => {
   if (i < 10) {
     i = "0" + i;
   }
   return i;
+}
+
+// Update steps 
+let tree = document.getElementById("tree");
+
+export const updateSteps = (progress) => {
+  if (display.on) {
+    switch (true) {
+      case progress < 33:
+        tree.href = "../resources/assets/healthy_growing.png";
+        break;
+      case progress < 66:
+        tree.href = "../resources/assets/after_2_weeks.png";
+        break;
+      case progress < 99:
+        tree.href = "../resources/assets/variation_1@4x.png";
+        break;
+      case progress >= 100:
+        tree.href = "../resources/assets/variation_2@4x.png";
+        break;
+      default:
+        tree.href = "../resources/assets/healthy_growing.png";
+        break;
+    }
+  }
 }
